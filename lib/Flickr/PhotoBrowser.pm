02006bff50fb2fe88d8de09494f40987342a1076
eballes <eballes@gmail.com>
2011-05-02 15:42:57 +0200

    Library and interfaced "shaped"

--- lib/Flickr/PhotoBrowser.pm
+++ lib/Flickr/PhotoBrowser.pm
@@ -8,12 +8,6 @@ use Carp;
 use Data::Dumper;
 use Flickr::API;
 
-# Key:
-#  e5df46b2cf8cfe59f58df245ec09c9d4
-#
-# Secret:
-#  e1b9e34909d1c9f2
-
 our $VERSION = '0.01';
 
 sub _getValues {
@@ -27,8 +21,6 @@ sub _getValues {
 
         shift @{$elements};
         if ( defined $elements->[0] && defined $field->{children} ) {
-
-            #print "Recursive call\n";
             return _getValues( $field, $elements, $attr );
         }
         else {
@@ -37,35 +29,15 @@ sub _getValues {
               : return $field->{children}[0]{content};
         }
     }
-
     return;
-}
-
-sub _getToken {
-    my ($self) = @_;
-    croak "OO use required\n"
-      if ( ref $self ) ne "Flickr::PhotoBrowser";
-
-    return unless -e $ENV{HOME} . "/.FlickrBrowser";
-
-    open my $fd, '<', $ENV{HOME} . "/.FlickrBrowser";
-    my $token = scalar <$fd>;
-    close $fd;
-
-    my $response =
-      $self->{api}
-      ->execute_method( 'flickr.auth.checkToken', { auth_token => $token } );
-    $response->{success}
-      ? return $token
-      : return;
 }
 
 sub new {
     my ( $class, $options ) = @_;
     my $api = new Flickr::API(
         {
-            'key'    => 'e5df46b2cf8cfe59f58df245ec09c9d4',
-            'secret' => 'e1b9e34909d1c9f2'
+            'key'    => $options->{key},
+            'secret' => $options->{secret},
         }
     );
 
@@ -74,33 +46,30 @@ sub new {
 }
 
 sub login {
-    my ($self) = @_;
+    my ($self, $token) = @_;
     croak "OO use required\n"
       if ( ref $self ) ne "Flickr::PhotoBrowser";
 
-    my $response = $self->{api}->execute_method('flickr.auth.getFrob');
-    my $frob = _getValues( $response->{tree}, ['frob'] );
+    if ( !$token ) {
+        my $response = $self->{api}->execute_method('flickr.auth.getFrob');
+        my $frob = _getValues( $response->{tree}, ['frob'] );
 
-    if ( !$self->_getToken() ) {
         chomp( my $browser = `which firefox` || `which google-chrome` );
         my $uri = $self->{api}->request_auth_url( 'read', $frob );
 
         system( $browser, $uri );
         print "Go to\n-> $uri\nand grant read access to PhotoBroser\n";
-        print "Press enter to continue when finished.\n";
+        print "Press enter to continue when finished.\n"; 
+        getc;
 
         $response =
           $self->{api}
           ->execute_method( 'flickr.auth.getToken', { frob => $frob } );
-        my $token = _getValues( $response->{tree}, [ 'auth', 'token' ] );
+        $token = _getValues( $response->{tree}, [ 'auth', 'token' ] );
 
-        open my $fd, '>', $ENV{HOME} . '/.FlickrBrowser';
-        print $fd $token;
-        close $fd;
     }
 
-    my $token = $self->_getToken();
-    $response =
+    my $response =
       $self->{api}
       ->execute_method( 'flickr.auth.checkToken', { auth_token => $token } );
 
@@ -110,18 +79,37 @@ sub login {
         [ 'username', 'nsid' ]
     );
 
-    return { user => $user, nsid => $nsid, token => $token };
+    $self->{user} = $user; 
+    $self->{nsid} = $nsid; 
+    $self->{token} = $token;
+
+    return $token;
+}
+
+sub checkToken {
+    my ($self, $token) = @_;
+    croak "OO use required\n"
+      if ( ref $self ) ne "Flickr::PhotoBrowser";
+
+    my $response =
+      $self->{api}
+      ->execute_method( 'flickr.auth.checkToken', { auth_token => $token } );
+    $token = undef unless $response->{success};
+
+    return $token;
 }
 
 sub getCollections {
-    my ( $self, $nsid, $col_id ) = @_;
+    my ( $self,  $col_id ) = @_;
     croak "OO use required\n"
       if ( ref $self ) ne "Flickr::PhotoBrowser";
+    croak "User needs to be logged in first"
+        unless $self->{nsid};
 
     my @collections = ();
     my $response =
       $self->{api}->execute_method( 'flickr.collections.getTree',
-        { user_id => $nsid, collection_id => $col_id } );
+        { user_id => $self->{nsid}, collection_id => $col_id } );
 
     foreach my $field ( @{ $response->{tree}{children} } ) {
         next unless $field->{name} && $field->{name} eq 'collections';
@@ -146,14 +134,16 @@ sub getCollections {
 }
 
 sub getSetsOfCollections {
-    my ( $self, $nsid, $col_id ) = @_;
+    my ( $self, $col_id ) = @_;
     croak "OO use required\n"
       if ( ref $self ) ne "Flickr::PhotoBrowser";
+    croak "User needs to be logged in first"
+        unless $self->{nsid};
     my @sets = ();
 
     my $response =
       $self->{api}->execute_method( 'flickr.collections.getTree',
-        { user_id => $nsid, collection_id => $col_id } );
+        { user_id => $self->{nsid}, collection_id => $col_id } );
 
     foreach my $field ( @{ $response->{tree}{children} } ) {
         next unless $field->{name} && $field->{name} eq 'collections';
@@ -177,14 +167,16 @@ sub getSetsOfCollections {
 }
 
 sub getPicturesOfSet {
-    my ( $self, $token, $set_id, $size, $page, $photos ) = @_;
+    my ( $self, $set_id, $size, $page, $photos ) = @_;
     croak "OO use required\n"
       if ( ref $self ) ne "Flickr::PhotoBrowser";
+    croak "User needs to be logged in first"
+        unless $self->{token};
 
     my $response = $self->{api}->execute_method(
         'flickr.photosets.getPhotos',
         {
-            auth_token  => $token,
+            auth_token  => $self->{token},
             photoset_id => $set_id,
             extras      => $size,
             per_page    => 500,
@@ -204,11 +196,9 @@ sub getPicturesOfSet {
                 title => $photo->{attributes}{title},
               };
         }
-        print scalar @{$photos};
-        getc;
         $field->{attributes}{pages} == $field->{attributes}{page}
           ? return $photos
-          : return $self->getPicturesOfSet( $token, $set_id, $size, ++$page,
+          : return $self->getPicturesOfSet( $set_id, $size, ++$page,
             $photos );
     }
 }
